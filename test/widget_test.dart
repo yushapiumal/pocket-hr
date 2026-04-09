e574efdc9834b7610e767ad8184c372fbc32f158
badtux <viraj@ceynet.asia>
2026-04-09 11:44:43 +0530

    Temp: save changes before merging

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:cn_pocket_hr/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( PocketHR());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
